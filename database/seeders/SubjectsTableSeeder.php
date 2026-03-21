f01f40967f14041154d7662645ec7a70372e5b97
rnk7112517 <rnk7112517@gmail.com>
2026-03-21 19:53:40 +0900

    シードを行いusersテーブルとsubjectsテーブルに初期データを登録する ■SubjectsTableSeederには「国語、数学、英語」のレコードを作成する 対応完了！

--- database/seeders/SubjectsTableSeeder.php
+++ database/seeders/SubjectsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;// ← これ追加！
 
 class SubjectsTableSeeder extends Seeder
 {
@@ -14,6 +15,20 @@ class SubjectsTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('subjects')->insert([
+        [
+        'subject' => '国語',
+        'created_at' => now(),
+        ],
+        [
+        'subject' => '数学',
+        'created_at' => now(),
+        ],
+        [
+        'subject' => '英語',
+        'created_at' => now(),
+        ],
+        ]);
         // 国語、数学、英語を追加
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+
+            'over_name' => '山田',
+            'under_name'=> '太郎',
+            'over_name_kana' => 'ヤマダ',
+            'under_name_kana' => 'タロウ',
+            'mail_address' => 'test@example.com',
+            'sex' => 1, // 男:1 女:2 など（仕様に合わせる）
+            'birth_day' => '2000-01-01',
+            'role' => 1, // 権限（仕様による）
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
 
     }
 }
