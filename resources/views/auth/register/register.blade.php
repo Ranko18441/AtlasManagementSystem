2c1143823b623cafccdfeac17cd19f6a1eb06376
rnk7112517 <rnk7112517@gmail.com>
2026-04-29 17:14:40 +0900

    バリデーションを訂正しました。

--- resources/views/auth/register/register.blade.php
+++ resources/views/auth/register/register.blade.php
@@ -5,68 +5,61 @@
         <div class="register_form">
           <div class="d-flex mt-3" style="justify-content:space-between">
             <div class="" style="width:140px">
+              @error('over_name')
+              <div class="error"><span>{{ $message }}</span></div>
+              @enderror
               <label class="d-block m-0" style="font-size:13px">姓</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 over_name" name="over_name">
-                <!-- 下にバリデーションのerrorを出すためのものを記述 -->
-                @error('over_name')
-                <div class="error"><span>{{ $message }}</span></div>
-                @enderror
-
               </div>
             </div>
             <div class="" style="width:140px">
+              @error('under_name')
+              <div class="error"><span>{{ $message }}</span></div>
+              @enderror
               <label class=" d-block m-0" style="font-size:13px">名</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 under_name" name="under_name">
-
-                <!-- 下にバリデーションのerrorを出すためのものを記述 -->
-                @error('under_name')
-                <div class="error"><span>{{ $message }}</span></div>
-                @enderror
-
               </div>
             </div>
           </div>
           <div class="d-flex mt-3" style="justify-content:space-between">
             <div class="" style="width:140px">
+              <!-- 下にバリデーションのerrorを出すためのものを記述 -->
+              @error('over_name_kana')
+              <div class="error"><span>{{ $message }}</span></div>
+              @enderror
               <label class="d-block m-0" style="font-size:13px">セイ</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 over_name_kana" name="over_name_kana">
-
-                <!-- 下にバリデーションのerrorを出すためのものを記述 -->
-                @error('over_name_kana')
-                <div class="error"><span>{{ $message }}</span></div>
-                @enderror
-
               </div>
             </div>
             <div class="" style="width:140px">
+              <!-- 下にバリデーションのerrorを出すためのものを記述 -->
+              @error('under_name_kana')
+              <div class="error"><span>{{ $message }}</span></div>
+              @enderror
               <label class="d-block m-0" style="font-size:13px">メイ</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 under_name_kana" name="under_name_kana">
-
-                <!-- 下にバリデーションのerrorを出すためのものを記述 -->
-                @error('under_name_kana')
-                <div class="error"><span>{{ $message }}</span></div>
-                @enderror
-
               </div>
             </div>
           </div>
           <div class="mt-3">
+            @error('mail_address')
+            <div class="error"><span>{{ $message }}</span></div>
+            @enderror
             <label class="m-0 d-block" style="font-size:13px">メールアドレス</label>
             <div class="border-bottom border-primary">
               <input type="mail" class="w-100 border-0 mail_address" name="mail_address">
-
-              <!-- 下にバリデーションのerrorを出すためのものを記述 -->
-                @error('mail_address')
-                <div class="error"><span>{{ $message }}</span></div>
-                @enderror
- 
             </div>
           </div>
         </div>
+
+        @error('sex')
+         <div class="error"><span>{{ $message }}</span></div>
+        @enderror
+
         <div class="mt-3">
           <input type="radio" name="sex" class="sex" value="1">
           <label style="font-size:13px">男性</label>
@@ -74,13 +67,12 @@
           <label style="font-size:13px">女性</label>
           <input type="radio" name="sex" class="sex" value="3">
           <label style="font-size:13px">その他</label>
+        </div>
 
-          @error('sex')
-                <div class="error"><span>{{ $message }}</span></div>
-                @enderror
-              
-              </div>
         <div class="mt-3">
+          @error('old_year')
+          <div class="error"><span>{{ $message }}</span></div>
+          @enderror
           <label class="d-block m-0 aa" style="font-size:13px">生年月日</label>
           <select class="old_year" name="old_year">
             <option value="none">-----</option>
@@ -111,10 +103,6 @@
             <option value="2009">2009</option>
             <option value="2010">2010</option>
           </select>
-          <!-- 下にバリデーションのerrorを出すためのものを記述 -->
-                @error('old_year')
-                <div class="error"><span>{{ $message }}</span></div>
-                @enderror
 
           <label style="font-size:13px">年</label>
           <select class="old_month" name="old_month">
@@ -132,10 +120,6 @@
             <option value="11">11</option>
             <option value="12">12</option>
           </select>
-          <!-- 下にバリデーションのerrorを出すためのものを記述 -->
-                @error('old_month')
-                <div class="error"><span>{{ $message }}</span></div>
-                @enderror
           <label style="font-size:13px">月</label>
           <select class="old_day" name="old_day">
             <option value="none">-----</option>
@@ -172,14 +156,14 @@
             <option value="31">31</option>
 
           </select>
-          @error('old_day')
-                <div class="error"><span>{{ $message }}</span></div>
-                @enderror
 
           <label style="font-size:13px">日</label>
 
         </div>
         <div class="mt-3">
+          @error('role')
+          <div class="error"><span>{{ $message }}</span></div>
+          @enderror
           <label class="d-block m-0" style="font-size:13px">役職</label>
           <input type="radio" name="role" class="admin_role role" value="1">
           <label style="font-size:13px">教師(国語)</label>
@@ -189,10 +173,6 @@
           <label style="font-size:13px">教師(英語)</label>
           <input type="radio" name="role" class="other_role role" value="4">
           <label style="font-size:13px" class="other_role">生徒</label>
-          <!-- 下にバリデーションのerrorを出すためのものを記述 -->
-                @error('role')
-                <div class="error"><span>{{ $message }}</span></div>
-                @enderror
         </div>
         <div class="select_teacher d-none">
           <label class="d-block m-0" style="font-size:13px">選択科目</label>
@@ -204,25 +184,21 @@
           @endforeach
         </div>
         <div class="mt-3">
+          @error('password')
+              <div class="error"><span>{{ $message }}</span></div>
+          @enderror
           <label class="d-block m-0" style="font-size:13px">パスワード</label>
           <div class="border-bottom border-primary">
             <input type="password" class="border-0 w-100 password" name="password">
-
-            @error('password')
-                <div class="error"><span>{{ $message }}</span></div>
-            @enderror
-
           </div>
         </div>
         <div class="mt-3">
+          @error('password_confirmation')
+              <div class="error"><span>{{ $message }}</span></div>
+          @enderror
           <label class="d-block m-0" style="font-size:13px">確認用パスワード</label>
           <div class="border-bottom border-primary">
             <input type="password" class="border-0 w-100 password_confirmation" name="password_confirmation">
-
-            @error('password')
-                <div class="error"><span>{{ $message }}</span></div>
-            @enderror
-
           </div>
         </div>
         <div class="mt-5 text-right">
