4165b9e17685060179ff1be0b592273bc71de94d
rnk7112517 <rnk7112517@gmail.com>
2026-05-03 18:39:33 +0900

    いいねの数を表示しました

--- app/Models/Posts/Like.php
+++ app/Models/Posts/Like.php
@@ -8,11 +8,13 @@ class Like extends Model
 {
     const UPDATED_AT = null;
 
+    // ↓は操作できるカラムの設定
     protected $fillable = [
         'like_user_id',
         'like_post_id'
     ];
 
+     // ↓$post_idを受け取って処理する　いいねした投稿のIDから取得してカウントしている。
     public function likeCounts($post_id){
         return $this->where('like_post_id', $post_id)->get()->count();
     }
