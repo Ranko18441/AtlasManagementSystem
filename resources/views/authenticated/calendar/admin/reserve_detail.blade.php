b4025389fc5d126f4fc7374ad9ec9435fc9d0272
rnk7112517 <rnk7112517@gmail.com>
2026-05-31 17:16:57 +0900

    @foreach($reservePersons as $reservePerson)を追記して予約詳細ページにユーザー情報を表示済み

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -4,13 +4,31 @@
     <p><span>日</span><span class="ml-3">部</span></p>
     <div class="h-75 border">
       <table class="">
+        @foreach($reservePersons as $reservePerson)
         <tr class="text-center">
           <th class="w-25">ID</th>
-          <th class="w-25">名前</th>
+        <td>
+        @foreach($reservePerson->users as $user)
+        {{ $user->id }}
+        @endforeach
+         </td>
         </tr>
+        @endforeach
+        
+
+        @foreach($reservePersons as $reservePerson)
         <tr class="text-center">
-          <td class="w-25"></td>
-          <td class="w-25"></td>
+        <th class="w-25">名前</th>
+        <td>
+          @foreach($reservePerson->users as $user)
+          {{ $user->over_name }} {{ $user->under_name }}
+          @endforeach
+        </td>
+      </tr>
+      @endforeach
+        <tr class="text-center">
+          <td class="w-25">場所</td>
+          <td class="w-25">リモート</td>
         </tr>
       </table>
     </div>
