118a614bff9078b6083ce187295a76efa690a74c
rnk7112517 <rnk7112517@gmail.com>
2026-06-13 18:45:23 +0900

    途中

--- resources/views/authenticated/bulletinboard/post_detail.blade.php
+++ resources/views/authenticated/bulletinboard/post_detail.blade.php
@@ -7,8 +7,11 @@
           <div>
           </div>
           <div>
+            <!-- 下記記述で自分以外は編集できないようにif文をつけた -->
+            @if (Auth::id() === $post->user_id)
             <span class="edit-modal-open" post_title="{{ $post->post_title }}" post_body="{{ $post->post }}" post_id="{{ $post->id }}">編集</span>
             <a href="{{ route('post.delete', ['id' => $post->id]) }}">削除</a>
+            @endif
           </div>
         </div>
 
